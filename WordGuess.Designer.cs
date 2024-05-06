d1190821526c506945730eec5fb23baf157c83bd
Murphy Schaff <murphy.schaff@gmail.com>
2024-05-05 19:25:23 -0600

    WordGuess: Added remaining functionalities

--- WordGuess.Designer.cs
+++ WordGuess.Designer.cs
@@ -30,8 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WordGuess));
             this.title = new System.Windows.Forms.Label();
-            this.resetButton = new System.Windows.Forms.Button();
             this.test = new System.Windows.Forms.Label();
+            this.feedback = new System.Windows.Forms.Label();
+            this.buttonBackground = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // title
@@ -44,37 +46,63 @@
             this.title.Size = new System.Drawing.Size(304, 55);
             this.title.TabIndex = 51;
             this.title.Text = "Word Guess!";
-            // 
-            // resetButton
-            // 
-            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resetButton.Location = new System.Drawing.Point(260, 450);
-            this.resetButton.Name = "resetButton";
-            this.resetButton.Size = new System.Drawing.Size(122, 45);
-            this.resetButton.TabIndex = 55;
-            this.resetButton.Text = "Reset";
-            this.resetButton.UseVisualStyleBackColor = true;
+            this.title.Click += new System.EventHandler(this.title_Click);
             // 
             // test
             // 
             this.test.AutoSize = true;
+            this.test.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.test.Location = new System.Drawing.Point(31, 30);
             this.test.Name = "test";
-            this.test.Size = new System.Drawing.Size(35, 13);
+            this.test.Size = new System.Drawing.Size(0, 20);
             this.test.TabIndex = 56;
-            this.test.Text = "label1";
+            // 
+            // feedback
+            // 
+            this.feedback.AutoSize = true;
+            this.feedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.feedback.Location = new System.Drawing.Point(12, 469);
+            this.feedback.Name = "feedback";
+            this.feedback.Size = new System.Drawing.Size(0, 24);
+            this.feedback.TabIndex = 57;
+            // 
+            // buttonBackground
+            // 
+            this.buttonBackground.AutoSize = true;
+            this.buttonBackground.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.buttonBackground.Location = new System.Drawing.Point(500, 455);
+            this.buttonBackground.MinimumSize = new System.Drawing.Size(150, 70);
+            this.buttonBackground.Name = "buttonBackground";
+            this.buttonBackground.Size = new System.Drawing.Size(150, 70);
+            this.buttonBackground.TabIndex = 59;
+            // 
+            // resetButton
+            // 
+            this.resetButton.AutoSize = true;
+            this.resetButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(512, 465);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Padding = new System.Windows.Forms.Padding(20, 10, 20, 10);
+            this.resetButton.Size = new System.Drawing.Size(126, 51);
+            this.resetButton.TabIndex = 60;
+            this.resetButton.Text = "Reset";
+            this.resetButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
             // WordGuess
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 508);
-            this.Controls.Add(this.test);
+            this.ClientSize = new System.Drawing.Size(650, 525);
             this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.buttonBackground);
+            this.Controls.Add(this.feedback);
+            this.Controls.Add(this.test);
             this.Controls.Add(this.title);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "WordGuess";
-            this.Text = "GamesForClass: WordGuess";
+            this.Text = "GamesForClass: Word Guess";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,7 +111,9 @@
         #endregion
 
         private System.Windows.Forms.Label title;
-        private System.Windows.Forms.Button resetButton;
         private System.Windows.Forms.Label test;
+        private System.Windows.Forms.Label feedback;
+        private System.Windows.Forms.Label buttonBackground;
+        private System.Windows.Forms.Label resetButton;
     }
 }
--- WordGuess.cs
+++ WordGuess.cs
@@ -16,9 +16,16 @@ namespace GamesForClass
     {
         public String[] words;
         public Label[,] lables = new Label[5, 5];
+
+        private int guess = 0;
+        private int index = 0;
+        private String word;
+        private bool play = true;
         public WordGuess()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyUp += new KeyEventHandler(WordGuess_KeyUp);
             String file = Resources.wordle_answers_alphabetical;
             words = file.Split('\n');
             placeLables();
@@ -43,6 +50,7 @@ namespace GamesForClass
                     label.Size = new Size(labelSize, labelSize);
                     label.BackColor = Color.Gray;
                     label.Font = new Font("Microsoft Sans Sarif", 50);
+                    label.TextAlign = ContentAlignment.MiddleCenter;
                     label.BringToFront();
 
                     lables[i,j] = label;
@@ -55,8 +63,152 @@ namespace GamesForClass
         private void chooseWord()
         {
             Random rnd = new Random();
-            String word = words[rnd.Next(words.Length)];
+            word = words[rnd.Next(words.Length)].ToUpper();
+        }
+        //keeps track of user input
+        private void WordGuess_KeyUp(object sender, KeyEventArgs e)
+        {
+            String entry = e.KeyCode.ToString();
+            Label label;
+            //makes sure the game is currently going
+            if (play)
+            {
+                feedback.Text = "";
+                //user wants to remove character
+                if (entry == "Back")
+                {
+                    if (index != 0)
+                    {
+                        index--; 
+                        label = lables[guess, index];
+                        label.Text = "";
+                    }
+                }
+                //user wants to enter guess
+                else if (entry == "Return")
+                {
+                    if (index == 5)
+                    {
+                        //checks guess validity
+                        if (checkGuess())
+                        {
+                            guess++;
+                            feedback.Text = "You guessed \"" + word + "\" in " + guess.ToString() + " guesses";
+                            play = false;
+                        }
+                        else
+                        {
+                            guess++;
+                            index = 0;
+                            //out of guesses, game over
+                            if (guess == 5)
+                            {
+                                feedback.Text = "You ran out of guesses.\nThe correct word was \"" + word + "\"";
+                                play = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        feedback.Text = "Please enter a full word";
+                    }
+                }
+                //user wants to enter character
+                else if (entry.Length == 1)
+                {
+                    if (index < 5)
+                    {
+                        label = lables[guess, index];
+                        label.Text = entry;
+                        index++;
+                    }
+                }
+                else
+                {
+                    //invalid input
+                    feedback.Text = "Invalid input. Please try again";
+                }
+            }
+        }
+        #region guess checking
+        //checks to see if the guess made by the user is correct
+        //returns true if so, false if not
+        public bool checkGuess()
+        {
+            Label label;
+            char currentLetter;
+            int correct = 0;
+            //checks if a letter is correct. If correct, marks space green
+            for (int i = 0; i < lables.GetLength(0); i++)
+            {
+                label = lables[guess, i];
+                currentLetter = (char)label.Text[0];
+                if (currentLetter == word[i])
+                {
+                    label.BackColor = Color.Green;
+                    label.ForeColor = Color.White;
+                    correct++;
+                }
+            }
+
+            if (correct == 5) return true; else checkInWord(); return false;
+        }
+        public void checkInWord()
+        {
+            Label label;
+            char currentletter;
+            //looks through each letter that is not correct, checks to see if it is in word (and spot is not currently correct)
+            for (int i = 0; i < lables.GetLength(0); i++)
+            {
+                label = lables[guess, i];
+                if (label.BackColor != Color.Green)
+                {
+                    currentletter = (char)label.Text[0];
+                    for (int j = 0; j < lables.GetLength(0);j++)
+                    {
+                        //if the letter is in the word, and the letter has not been correctly guessed yet, then mark the space as yellow
+                        if (currentletter == word[j] && lables[guess, j].BackColor != Color.Green)
+                        {
+                            label.BackColor = Color.Yellow;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+        #region clear functions
+        //clears all lable back colors, and resets forecolor
+        public void resetLabels()
+        {
+            for (int i = 0; i < lables.GetLength(0); i++)
+            {
+                for (int j =0; j < lables.GetLength(0); j++)
+                {
+                    lables[i, j].BackColor = Color.Gray;
+                    lables[i, j].ForeColor = Color.Black;
+                    lables[i, j].Text = "";
+                }
+            }
+        }
+        #endregion
+
+        #region buttons
+        //resets the game, at any time
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            resetLabels();
+            chooseWord();
+            play = true;
+            guess = 0;
+            index = 0;
+            feedback.Text = "";
+            test.Text = "";
+        }
+        private void title_Click(object sender, EventArgs e)
+        {
             test.Text = word;
         }
+        #endregion
+
     }
 }
